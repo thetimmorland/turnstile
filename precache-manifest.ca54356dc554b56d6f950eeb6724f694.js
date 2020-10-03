7af7efcdb52c12fb76858441f01e94083e49a3be
thetimmorland <thetimmorland@users.noreply.github.com>
2020-10-03 18:50:58 +0000

    deploy: 94e9c46560eb0695bf66fcac34685bae1c4a6ad1

---
+++ precache-manifest.ca54356dc554b56d6f950eeb6724f694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633e39278034124c6d90b8375aacc4d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f40f3b9ad0377d795d1",
+    "url": "/static/css/main.3faa8f6a.chunk.css"
+  },
+  {
+    "revision": "bc4aacd709065f997256",
+    "url": "/static/js/2.9f0c8c41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9f0c8c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f40f3b9ad0377d795d1",
+    "url": "/static/js/main.f51a02f0.chunk.js"
+  },
+  {
+    "revision": "6a64de495ebe254dac7b",
+    "url": "/static/js/runtime-main.6935c06a.js"
+  }
+]);
