03d81d9236d0f8f9a5dd81c894584cc2e0bdb239
Timothy Morland <me@timothy-morland.com>
2020-11-11 12:13:50 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Turnstile
+
+[![Deploy](https://github.com/thetimmorland/turnstile/workflows/Deploy/badge.svg)](https://thetimmorland.github.io/turnstile)
+
+A browser based proof editor built on top of
+https://github.com/reduxjs/cra-template-redux-typescript
---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+
+test("it renders", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+});
